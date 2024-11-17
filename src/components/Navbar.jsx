4dae328d04fdfa44d701bab445eb6a67f9027631
94Jesse <jesselu94@gmail.com>
2024-11-17 03:20:57 -0600

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+// // Navbar.jsx
+// import React from "react";
+
+// function Navbar() {
+//   return <div>Ripple & Refresh</div>;
+// }
+
+// export default Navbar;
+
+ 
+import React from 'react'
+ 
+function FeaturedItem() {
+  return (
+    <div>FeaturedItem</div>
+  )
+}
+ 
+export default FeaturedItem
